639a032f707d5aa4f92c71a350f9bafedb3fc864
michaelhaaf <michael.haaf@gmail.com>
2021-10-29 20:25:27 -0400

    hw6: finished reddit authorization

--- hw6/submission_template/src/collect.py
+++ hw6/submission_template/src/collect.py
@@ -4,36 +4,38 @@ import requests.auth
 import os, sys
 
 # TODO: Hash these and unhash them to send
-CLIENT_ID = "S-5YZ5hVQrvZzV8rogZsOQ"
-CLIENT_SECRET = "QMHIOc5zwD70iQcuTVHJBhzQyTs8Xg"
-CLIENT_UN = "williashatner"
-CLIENT_PW = "q04qPk1ZjtZd"
+CLIENT_ID = "5f5qHORUv3wF94Djz7QFfQ"
+CLIENT_SECRET = "qlYhkEfImnvpVW9vFmmzc0B07KwR_Q"
+CLIENT_UN = "FairNeedleworker9516"
+CLIENT_PW = "JLRMmZ'a0;W6fC?X)B*w/Qt&a"
 
-BASE_URL = "https://www.reddit.com/r/aww/new.json"
+BASE_URL = "https://oauth.reddit.com/r/aww/new.json"
 CACHE_FILE = "new.cache.json"
 
 
 def auth_request():
     client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
-    post_data = {"grant_type": "password", "username": CLIENT_UN, "password": "snoo"}
-    headers = {"User-Agent": "ChangeMeClient/0.1 by YourUsername"}
+    post_data = {"grant_type": "password", "username": CLIENT_UN, "password": CLIENT_PW}
+    headers = {"User-Agent": f"hw6-script by {CLIENT_UN}"}
     response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
     return response
 
 
-def reddit_request():
-    auth_obj = auth_request()
-    #TODO use the auth obj
-    headers = {"Authorization": "bearer fhTdafZI-0ClEzzYORfBSCR7x3M", "User-Agent": "ChangeMeClient/0.1 by YourUsername"}
-    response = requests.get("https://oauth.reddit.com/api/v1/me", headers=headers)
+def reddit_request(auth_obj):
+    print(f'Fetching from {BASE_URL}')
+    print(f'Using auth_obj {auth_obj}')
+    token_type = auth_obj['token_type']
+    access_token = auth_obj['access_token']
+    headers = {"Authorization": f"{token_type} {access_token}", "User-Agent": f"hw6-script by {CLIENT_UN}"}
+    response = requests.get(BASE_URL, headers=headers)
+    return response
+
 
 def main():
-    url = BASE_URL
-    print(f'Getting url={url}')
 
     if not os.path.exists(CACHE_FILE):
-        print(f'Fetching from {url}')
-        r = requests.get(url)
+        auth_obj = auth_request()
+        r = reddit_request(auth_obj.json())
         root_element = r.json()
         with open(CACHE_FILE, 'w') as fh:
             json.dump(root_element, fh)
