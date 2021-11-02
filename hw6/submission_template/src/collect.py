2f2f28d8ce1a0e43b363ffac12f7d6d74e2e0a98
michaelhaaf <michael.haaf@gmail.com>
2021-11-02 12:25:13 -0400

    hw6: all three scripts finished

--- hw6/submission_template/src/collect.py
+++ hw6/submission_template/src/collect.py
@@ -9,8 +9,13 @@ CLIENT_SECRET = "qlYhkEfImnvpVW9vFmmzc0B07KwR_Q"
 CLIENT_UN = "FairNeedleworker9516"
 CLIENT_PW = "JLRMmZ'a0;W6fC?X)B*w/Qt&a"
 
-BASE_URL = "https://oauth.reddit.com/r/aww/new.json"
-CACHE_FILE = "new.cache.json"
+BASE_URL = "https://oauth.reddit.com/r/"
+SAMPLE1_CACHE = "sample1.json"
+SAMPLE2_CACHE = "sample2.json"
+
+SAMPLE1_SUBREDDITS = ["funny", "AskReddit", "gaming", "aww", "pics", "Music", "science", "worldnews", "videos", "todayilearned"]
+SAMPLE2_SUBREDDITS = ["memes", "AskReddit", "politics", "nfl", "nba", "wallstreetbets", "teenagers", "PublicFreakout", "leagueoflegends", "unpopularopinion"]
+NUM_POSTS = 100
 
 
 def auth_request():
@@ -21,33 +26,36 @@ def auth_request():
     return response
 
 
-def reddit_request(auth_obj):
-    print(f'Fetching from {BASE_URL}')
-    print(f'Using auth_obj {auth_obj}')
+def reddit_request(auth_obj, url):
+    print(f'Fetching from {url} using auth_obj {auth_obj["access_token"]}')
     token_type = auth_obj['token_type']
     access_token = auth_obj['access_token']
     headers = {"Authorization": f"{token_type} {access_token}", "User-Agent": f"hw6-script by {CLIENT_UN}"}
-    response = requests.get(BASE_URL, headers=headers)
+    payload = {"limit": NUM_POSTS, "type": "link"}
+    response = requests.get(url, headers=headers, params=payload)
     return response
 
 
-def main():
-
-    if not os.path.exists(CACHE_FILE):
-        auth_obj = auth_request()
-        r = reddit_request(auth_obj.json())
+def get_top_posts_from_subreddits(subreddits):
+    posts = []
+    auth_obj = auth_request()
+    for subreddit in subreddits:
+        r = reddit_request(auth_obj.json(), BASE_URL + subreddit + "/new.json")
         root_element = r.json()
-        with open(CACHE_FILE, 'w') as fh:
-            json.dump(root_element, fh)
-    else:
-        print('Loading from cache')
-        with open(CACHE_FILE, 'r') as fh:
-            root_element = json.load(fh)
+        for post in root_element['data']['children']:
+            posts.append(post)
+    return posts
 
-    posts = root_element['data']['children']
 
-    for post in posts:
-            print(post['data']['title'])
+def dump_top_posts_from_subreddits_to_file(filename, subreddits):
+    posts = get_top_posts_from_subreddits(subreddits)
+    with open(filename, 'w') as fh:
+        fh.write("\n".join(json.dumps(post) for post in posts))
+
+def main():
+    dump_top_posts_from_subreddits_to_file(SAMPLE1_CACHE, SAMPLE1_SUBREDDITS)
+    dump_top_posts_from_subreddits_to_file(SAMPLE2_CACHE, SAMPLE2_SUBREDDITS)
+
 
 if __name__ == '__main__':
     main()
