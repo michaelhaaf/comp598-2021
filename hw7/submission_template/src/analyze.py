163a7189f2cc5018011ea6b55774e722267bcd67
michaelhaaf <michael.haaf@gmail.com>
2021-11-05 14:47:27 -0400

    hw7: finished

--- hw7/submission_template/src/analyze.py
+++ hw7/submission_template/src/analyze.py
@@ -8,14 +8,24 @@ def main(args):
     # process input file
     print(f"Loading annotations from {args.coded_file}...")
     with open(args.coded_file, "r") as in_file:
-        input_data = pd.read_csv(in_file, sep='\t', header=0)
+        df = pd.read_csv(in_file, sep='\t', header=0)
 
 
     # count annotations
+    output_dict = {}
+    output_dict['course-related'] = int(df['coding'].value_counts().get('c', 0))
+    output_dict['food-related'] = int(df['coding'].value_counts().get('f', 0))
+    output_dict['residence-related'] = int(df['coding'].value_counts().get('r', 0))
+    output_dict['other'] = int(df['coding'].value_counts().get('o', 0))
 
     # print to file/stddout
+    if args.out_file:
+        with open(args.out_file, "w") as out_file:
+            json.dump(output_dict, out_file)
+        print(f"Printed output to {args.out_file}")
+    else:
+        print(output_dict)
 
-    print("Finished!")
     
 
 ## Usage
@@ -30,6 +40,7 @@ if __name__ == "__main__":
             dest='out_file'
             )
     parser.add_argument('-i',
+            required=True,
             help='input annotated tsv file',
             type=str,
             dest='coded_file'
