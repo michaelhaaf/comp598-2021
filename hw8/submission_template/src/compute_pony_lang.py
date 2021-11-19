cf34c74cb0509e006bd84c273d679d1e7f143bb6
michaelhaaf <michael.haaf@gmail.com>
2021-11-19 16:45:56 -0500

    hw8: i think I'm finished

--- hw8/submission_template/src/compute_pony_lang.py
+++ hw8/submission_template/src/compute_pony_lang.py
@@ -5,7 +5,6 @@ import math
 
 
 def idf(word, pony_counts):
-    # in progress
     ponies_that_used_word_count = sum([1 for pony in pony_counts if word in pony_counts[pony]])
     return math.log10( len(pony_counts.keys()) / ponies_that_used_word_count )
 
@@ -14,13 +13,16 @@ def top_n_tf_idf(pony, pony_counts, n):
     tf_idf_scores = { word: count * idf(word, pony_counts) for word, count in pony_counts[pony].items() }
     return sorted(tf_idf_scores, key=lambda x: x[1], reverse=True)[0:n]
 
+def compute_pony_lang(pony_counts, num_words): 
+    return {pony: top_n_tf_idf(pony, pony_counts, num_words) for pony in pony_counts.keys()}
+
 
 def main(args):
     in_file = open(args.pony_counts, "r")
     pony_counts = json.load(in_file)
     in_file.close()
 
-    results = {pony: top_n_tf_idf(pony, pony_counts, args.num_words) for pony in pony_counts.keys()}
+    results = compute_pony_lang(pony_counts, args.num_words)
 
     print(json.dumps(results, indent=4))
 
@@ -28,7 +30,7 @@ def main(args):
 # USAGE:
 # python compute_pony_lang.py -c <word_counts_json> -n <num_words>
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='compute frequenc and distinctive pony language use')
+    parser = argparse.ArgumentParser(description='compute frequency and distinctive pony language use')
     parser.add_argument('-c', 
                         dest='pony_counts',
                         required=True,
--- hw8/submission_template/test/test_tasks.py
+++ hw8/submission_template/test/test_tasks.py
@@ -5,7 +5,10 @@ parentdir = Path(__file__).parents[1]
 sys.path.append(parentdir)
 
 from src.compile_word_counts import load_data, compile_word_counts
+from src.compute_pony_lang import compute_pony_lang, idf
+
 import pandas
+import math
 import json
 
 class TasksTest(unittest.TestCase):
@@ -25,24 +28,44 @@ class TasksTest(unittest.TestCase):
         
 
     def test_task1(self):
-        # use self.mock_dialog and self.true_word_counts; REMOVE self.assertTrue(True) and write your own assertion, i.e. self.assertEquals(...)
-        
         # setup
         df = load_data(self.mock_dialog)
 
         # test
         result = compile_word_counts(df)
 
-        print(result)
-        print(self.true_word_counts_json)
         # assert
         self.assertEqual(result, self.true_word_counts_json)
 
 
     def test_task2(self):
-        # use self.true_word_counts self.true_tf_idfs; REMOVE self.assertTrue(True) and write your own assertion, i.e. self.assertEquals(...)
-        self.assertTrue(True)
-        
-    
+
+        # test
+        result = compute_pony_lang(self.true_word_counts_json, 2)
+
+        # assert
+        self.assertEqual(result, self.true_tf_idfs_json)
+
+        # extra idf computation tests
+        please_idf = idf("please", self.true_word_counts_json) 
+        pretty_idf = idf("pretty", self.true_word_counts_json) 
+        princess_idf = idf("princess", self.true_word_counts_json) 
+        note_idf = idf("note", self.true_word_counts_json) 
+        oyes_idf = idf("oyes", self.true_word_counts_json) 
+        okie_idf = idf("okie", self.true_word_counts_json) 
+        aaaaaaaa_idf = idf("aaaaaaaa", self.true_word_counts_json) 
+        lol_idf = idf("lol", self.true_word_counts_json) 
+
+        # assert 
+        self.assertEqual(please_idf, math.log10( 6 / 2 ) )
+        self.assertEqual(pretty_idf, math.log10( 6 / 2 ))
+        self.assertEqual(princess_idf, math.log10( 6 / 1 ))
+        self.assertEqual(note_idf, math.log10( 6 / 1 ))
+        self.assertEqual(oyes_idf, math.log10( 6 / 4 ))
+        self.assertEqual(okie_idf, math.log10( 6 / 1 ))
+        self.assertEqual(aaaaaaaa_idf, math.log10( 6 / 1 ))
+        self.assertEqual(lol_idf, math.log10( 6 / 1 ))
+
+
 if __name__ == '__main__':
     unittest.main()
